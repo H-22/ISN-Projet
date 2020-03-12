2856f97be2a74b120ef5c197268e0f6359b3af3e
Ratelu <60732989+Ratelu@users.noreply.github.com>
2020-03-12 10:04:11 +0100

    Update thosmastest.js

--- thosmastest.js
+++ thosmastest.js
@@ -1,28 +1,34 @@
-var currentTheme = '';
-
-'night' {
-
-      backgroundColor:'#000000',
-
-      textColor: '#FFFFFF',
-
+<script type="text/javascript">
+  
+      
+function switchStyle(){
+ var style = document.getElementById("mycss").getAttribute("href")
+    if(style == "style.css"){
+       document.getElementById("mycss").setAttribute("href", "style2.css");
+    } 
+    else{
+       document.getElementById("mycss").setAttribute("href", "style.css");
     }
 
-'day':  {
-
-      backgroundColor:'#FFFFFF',
-
-      textColor: '#000000',
-
-    }
-
-
-  if {
-
-    setTheme('day');
-
-  } else {
-
-    setTheme('night');
-
-  }
+      
+      
+</script>
+  
+<link rel="stylesheet" type="text/css" href="style.css" id="mycss" />
+  
+ 
+      
+  
+<!-- bouton jour / nuit -->
+  
+     <div>
+  
+     <label class="switch">
+  
+  <input onchange="switchStyle()" type="checkbox">
+  
+   <div class="slider round"></div>
+  
+    </label>
+  
+    </div>
