d1b4c02c82ec7631386f407abb3a407ce446aef7
Ratelu <60732989+Ratelu@users.noreply.github.com>
2020-04-08 21:21:21 +0200

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,98 @@
+<div class="Test-question" data-id="70028" data-type="question" data-question-count="1" data-step="0">
+    
+            
+            <div class="Test-choice">
+
+                <div class="TestChoice TestChoice--qcm">
+
+                                            <div class="TestChoice-title Claimant-title"> Quelle style de film vouler vous voir?</div>
+                                            
+                    <ul class="TestChoice-list">
+
+                                                                                <li class="TestChoice-item TestChoice-item--active">
+                                <button data-id="70213" data-value="314">
+                                                                            Paisible.
+                                    
+                                </button>
+                            </li>
+                                                                                <li class="TestChoice-item">
+                                <button data-id="70215" data-value="315">
+                                                                            Violent.
+                                    
+                                </button>
+                            </li>
+                                                                                <li class="TestChoice-item">
+                                <button data-id="70212" data-value="313">
+                                                                            Triste.
+                                    
+                                </button>
+                            </li>
+                                                                                <li class="TestChoice-item">
+                                <button data-id="70214" data-value="312">
+                                                                            Sans préférence.
+                                    
+                                </button>
+                            </li>
+                        
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="Test-question" hidden="hidden" data-id="70193" data-type="question" data-question-count="2" data-step="1">
+                                     
+
+            <div class="Test-choice">
+
+                <div class="TestChoice TestChoice--qcm">
+
+                                            <div class="TestChoice-title Claimant-title"> Que preferez vous comme ambiance? </div>
+                                            
+                    <ul class="TestChoice-list">
+
+                                                                                <li class="TestChoice-item">
+                                <button data-id="70216" data-value="313">
+                                                                            Sombre.
+                                    
+                                </button>
+                            </li>
+                                                                                <li class="TestChoice-item">
+                                <button data-id="70217" data-value="314">
+                                                                            Légers.
+                                    
+                                </button>
+                            </li>
+                                                                                <li class="TestChoice-item">
+                                <button data-id="70219" data-value="315">
+                                                                            Nature.
+                                    
+                                </button>
+                            </li>
+                                                                                <li class="TestChoice-item">
+                                <button data-id="70218" data-value="312">
+                                                                            Sans préférence.
+                                    
+                                </button>
+                            </li>
+                        
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
